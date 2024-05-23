4bfd9b381f7773359cc91430e52c4e3a4904a262
Yazan Essam <yazanessam12@gmail.com>
2024-05-23 06:16:25 +0300

    Attacks Module UI is Done :)

--- src/renderer/views/project/attacks/exposedPanels.tsx
+++ src/renderer/views/project/attacks/exposedPanels.tsx
@@ -1,23 +1,22 @@
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable react/jsx-no-useless-fragment */
-/* eslint-disable react-hooks/rules-of-hooks */
 import { Loader2 } from 'lucide-react';
 import { useState } from 'react';
-import { Button } from '../../../components/ui/button';
 import { ProjectDetails } from '../../../types';
+import { Button } from '../../../components/ui/button';
 import { toast } from '../../../components/ui/use-toast';
 
-export default function PotentialXss(details: ProjectDetails) {
+export default function ExposedPanels(details: ProjectDetails) {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [Loading, setLoading] = useState<boolean>(false);
-  const RunPotentialXss = async () => {
+  const RunExposedPanels = async () => {
+    // eslint-disable-next-line react/destructuring-assignment
     if (details.name) {
-      const res = await window.electron.ipcRenderer.invoke('potential-xss', {
+      const res = await window.electron.ipcRenderer.invoke('exposed-panels', {
+        // eslint-disable-next-line react/destructuring-assignment
         projectName: details.name,
       });
       if (res) {
         toast({
-          title: 'Potential XSS job compeleted',
+          title: 'Exposed Panels job compeleted',
         });
       }
     }
@@ -27,7 +26,7 @@ export default function PotentialXss(details: ProjectDetails) {
     // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
       {!Loading ? (
-        <Button onClick={RunPotentialXss}>Process</Button>
+        <Button onClick={RunExposedPanels}>Process</Button>
       ) : (
         <Button disabled>
           <Loader2 className="mr-2 h-4 w-4 animate-spin" />
